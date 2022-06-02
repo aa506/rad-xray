6316fa07ab4833a5ce28f20be4e7d1b79b6e6d88
aa506 <29257678+aa506@users.noreply.github.com>
2022-06-02 10:09:19 +0800

    添加指定命令

--- browserscan.py
+++ browserscan.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 # coding: utf-8
 import subprocess
+import sys
 
 def main(data1):
 	target = data1
-	cmd = ["./xray.exe","webscan","--browser-crawler",target,"--html-output" ,"report__datetime__.html"]
+	cmd = ["/usr/bin/xray","webscan","--browser-crawler",target,"--html-output" ,"report__datetime__.html"]
 	rsp=subprocess.Popen(cmd)
 	output, error = rsp.communicate()
 	print(output)
 
+file1 = sys.argv[1]
 if __name__=='__main__':
-	file = open("url.txt")
+	file = open(file1)
 	for text in file.readlines():
 		data1=text.strip('\n')
 		main(data1)
